3811ea5a9a46e36222a7e95089045f6794c0a37c
alextdart <adart075@gmail.com>
2024-12-03 21:04:49 -0800

    fixed marker not showing up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { getDistance } from "geolib";
 import imageData from "./images.json";
+import L from "leaflet";
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 const ImageDisplay = ({ image, onNext }) => {
     return (
